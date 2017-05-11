24a724760b90979497484ad840745f8b96fc3418
dpfaffenbauer <dominik@lineofcode.at>
2017-05-11 18:25:31 +0200

    [Refactor] move all to src/CoreShop folder

---
+++ Form/Type/Rule/Action/ShippingRuleActionConfigurationType.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Bundle\ShippingBundle\Form\Type\Rule\Action;
+
+use CoreShop\Bundle\ShippingBundle\Form\Type\ShippingRuleChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ShippingRuleActionConfigurationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('shippingRule', TextType::class, [ //TODO: Should be ShippingRuleChoiceType, but would't save ID to database, instead it saves the whole object
+                'constraints' => [
+                    new NotBlank(['groups' => ['coreshop']]),
+                ],
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'coreshop_shipping_rule_action_shipping_rule';
+    }
+}
---
+++ Processor/CartCarrierProcessor.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Bundle\ShippingBundle\Processor;
+
+use CoreShop\Bundle\ShippingBundle\Checker\CarrierShippingRuleCheckerInterface;
+use CoreShop\Component\Address\Model\AddressInterface;
+use CoreShop\Component\Resource\Repository\RepositoryInterface;
+use CoreShop\Component\Shipping\Model\ShippableInterface;
+use CoreShop\Component\Shipping\Model\ShippingRuleGroupInterface;
+
+class CartCarrierProcessor implements CartCarrierProcessorInterface
+{
+    /**
+     * @var RepositoryInterface
+     */
+    private $carrierRepository;
+
+    /**
+     * @var CarrierShippingRuleCheckerInterface
+     */
+    private $carrierShippingRuleChecker;
+
+    /**
+     * @param RepositoryInterface                 $carrierRepository
+     * @param CarrierShippingRuleCheckerInterface $carrierShippingRuleChecker
+     */
+    public function __construct(
+        RepositoryInterface $carrierRepository,
+        CarrierShippingRuleCheckerInterface $carrierShippingRuleChecker
+    ) {
+        $this->carrierRepository = $carrierRepository;
+        $this->carrierShippingRuleChecker = $carrierShippingRuleChecker;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCarriersForCart(ShippableInterface $shippable, AddressInterface $address = null)
+    {
+        $carriers = $this->carrierRepository->findAll(); //TODO: restrict to store, but needs to be moved to CoreBundle to make it work: NOTE: Really? Shipping Rule Stores?!
+        $availableCarriers = [];
+
+        //First: Get all available carriers
+        foreach ($carriers as $carrier) {
+            if ($this->carrierShippingRuleChecker->isShippingRuleValid($carrier, $shippable, $address) instanceof ShippingRuleGroupInterface) {
+                $availableCarriers[] = $carrier;
+            }
+        }
+
+        return $availableCarriers;
+    }
+}
